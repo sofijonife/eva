c7ab9c61e9b3a9571b8f49ee594b8f23530c7010
rahdhani <140997505+rahdhani@users.noreply.github.com>
2023-08-01 14:11:44 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://cpmqueen.com/` 
+					`https://techhubparas.blogspot.com/` 
 				}`,
 			},
 		};
